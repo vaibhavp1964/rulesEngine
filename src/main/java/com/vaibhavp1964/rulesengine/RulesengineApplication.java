24daff70048e597c34b81b8fbf9949baf5ccc5e9
Vaibhav Pandey <vaibhav.pandey1964@gmail.com>
2023-06-13 21:39:43 +0530

    Initial commit

---
+++ src/main/java/com/vaibhavp1964/rulesengine/RulesengineApplication.java
@@ -0,0 +1,13 @@
+package com.vaibhavp1964.rulesengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RulesengineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RulesengineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vaibhavp1964/rulesengine/RulesengineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vaibhavp1964.rulesengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RulesengineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
